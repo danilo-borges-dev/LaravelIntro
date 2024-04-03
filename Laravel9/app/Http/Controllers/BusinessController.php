68787f815a7d1f7f13a1043a9debd69c6b98b1d4
Danilo Borges <danilosilva2017@hotmail.com>
2024-04-03 06:10:07 -0300

    Primeira Consulta com ORM Eloquent

--- Laravel9/app/Http/Controllers/BusinessController.php
+++ Laravel9/app/Http/Controllers/BusinessController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Business;
 use Illuminate\Http\Request;
 
 class BusinessController extends Controller
 {
-    //
+    public function index()
+    {
+        $businesses = Business::all();
+
+        $business = Business::find(1);
+
+        $whereBusiness = Business::where('name', 'Adams-Deckow')
+            ->where('email', 'ahintz@example.com')
+            ->get();
+
+        $whereFirstBusiness = Business::where('name', 'Adams-Deckow')
+            ->first();
+
+        dd($businesses, $business, $whereBusiness, $whereFirstBusiness);
+    }
 }
--- Laravel9/routes/web.php
+++ Laravel9/routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\BusinessController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/user/{user?}', [UserController::class, 'show']);
-
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/business', [BusinessController::class, 'index']);
 
 
 
